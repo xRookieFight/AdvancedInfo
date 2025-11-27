5ab1933d568060d027adac3f3fe83a0812121142
xRookieFight <50714128+xRookieFight@users.noreply.github.com>
2025-11-27 21:35:37 +0300

    Optimized getter os functions and added Linux to the OS list

--- src/main/kotlin/com/xrookiefight/Main.kt
+++ src/main/kotlin/com/xrookiefight/Main.kt
@@ -1,6 +1,7 @@
 package com.xrookiefight
 
 import cn.nukkit.Player
+import cn.nukkit.network.protocol.types.Platform
 import cn.nukkit.plugin.PluginBase
 import com.xrookiefight.command.PlayerInfoCommand
 import com.xrookiefight.objects.PlayerData
@@ -17,32 +18,12 @@ class Main : PluginBase() {
                 player.address,
                 player.loginChainData.deviceId,
                 player.loginChainData.deviceModel,
-                getPlayerOS(player.loginChainData.deviceOS),
+                Platform.getPlatformByID(player.loginChainData.deviceOS).name,
                 player.languageCode.name,
                 player.xuid,
                 player.loginChainData.clientUUID,
                 player.loginChainData.clientId
             )
-        }
-
-        fun getPlayerOS(deviceOS: Int) : String {
-            return when (deviceOS) {
-                -1 -> "Unknown"
-                1 -> "Android"
-                2 -> "IOS"
-                3 -> "MacOS"
-                4 -> "Amazon"
-                5 -> "Samsung Gear VR"
-                6 -> "Microsoft HoloLens"
-                7 -> "Windows 10"
-                8 -> "Windows 11"
-                9 -> "Dedicated"
-                10 -> "TvOS"
-                11 -> "PlayStation"
-                12 -> "Nintendo"
-                13 -> "Xbox"
-                else -> "Windows Phone"
-            }
         }
     }
 
